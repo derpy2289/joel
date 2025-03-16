d850bd8b79270c1dbae254d2634c25142cc15e90
derpy2289 <79153210+derpy2289@users.noreply.github.com>
2025-03-16 19:35:22 -0400

    Add files via upload

---
+++ joel2/godot-click-through-transparent-window-godot-4/cowbox.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+
+## A Godot object that moves randomly around the screen.
+
+
+@onready var _ClickPolygon2: CollisionPolygon2D = $"%ClickPolygon"
+
+	
+func _physics_process(_delta: float) -> void:
+	pass
